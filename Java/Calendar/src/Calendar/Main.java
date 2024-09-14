356a613bee568939182cd32a88d7e8319802d853
Alexis Simmons <absimmons@my365.bellevue.edu>
2024-09-14 14:14:23 -0400

    Uploading Calendar Management App

---
+++ Java/Calendar/src/Calendar/Main.java
@@ -0,0 +1,32 @@
+package Calendar;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.time.LocalDate;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame("Calendar");
+		frame.setSize(900, 500);
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setBackground(Color.white);
+		
+		JPanel mainPanel = new JPanel(new GridLayout(1, 2));
+		
+		LocalDate date = LocalDate.now();
+		Database database = new Database();
+		
+		mainPanel.add(new Calendar(date.getYear(), date.getMonthValue(), date, mainPanel, database));
+		mainPanel.add(new Events(date, database, mainPanel));
+		
+		frame.getContentPane().add(mainPanel);
+		frame.setVisible(true);
+		
+	}
+
+}
